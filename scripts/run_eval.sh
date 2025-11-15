2a3ca6431885eae875238d951248be5f16586ec5
Salman Shahid <salmanshahidzia@gmail.com>
2025-11-15 06:04:24 +0000

    new run_eval

--- scripts/run_eval.sh
+++ scripts/run_eval.sh
@@ -11,41 +11,54 @@ export B1K_EVAL_TIME=true;
 
 ########################################################
 #
-# Will keep the default 10 diffusion steps but using receeding_horizon control mode
+# Checking the beast checkpoint at 83k steps vs 66k steps
 #
 ########################################################
 
 aws s3 sync \
-    s3://behavior-challenge/outputs/checkpoints/pi05_b1k_oversample/openpi_05_20251114_055221/69000/ \
-    /workspace/openpi/outputs/checkpoints/pi05_b1k_oversample/openpi_05_20251114_055221/69000/
+    s3://behavior-challenge/outputs/checkpoints/pi05_b1k_22_TASKS_oversample/openpi_05_20251113_045215/83000/ \
+    /workspace/openpi/outputs/checkpoints/pi05_b1k_22_TASKS_oversample/openpi_05_20251113_045215/83000/
 
-EXP_NAME="openpi_05_20251114_055221_69k_steps"
+EXP_NAME="openpi_05_20251113_045215_83k_steps"
 LOG_DIR="video_outputs/${EXP_NAME}"
 
 mkdir -p "${LOG_DIR}"
 
-POLICY_ARGS="policy=local policy_config=pi05_b1k_22_TASKS_oversample policy_dir=/workspace/openpi/outputs/checkpoints/pi05_b1k_oversample/openpi_05_20251114_055221/69000/"
+POLICY_ARGS="policy=local policy_config=pi05_b1k_22_TASKS_oversample policy_dir=/workspace/openpi/outputs/checkpoints/pi05_b1k_22_TASKS_oversample/openpi_05_20251113_045215/83000/"
 
 XLA_PYTHON_CLIENT_PREALLOCATE=false python OmniGibson/omnigibson/learning/eval.py \
     ${POLICY_ARGS} \
     task.name=moving_boxes_to_storage \
     eval_on_train_instances=false \
-    eval_instance_ids=[0,1,2,3,4,5,6] \
+    eval_instance_ids=[2,3,4,5] \
     use_heavy_robot=true \
-    max_steps=5400 \
+    max_steps=7200 \
     inf_time_proprio_dropout=0.0 \
     num_diffusion_steps=10 \
     control_mode=receeding_horizon \
     log_path="${LOG_DIR}/test_10_steps_receeding_horizon"
 
-# XLA_PYTHON_CLIENT_PREALLOCATE=false python OmniGibson/omnigibson/learning/eval.py \
-#     ${POLICY_ARGS} \
-#     task.name=moving_boxes_to_storage \
-#     eval_on_train_instances=true \
-#     eval_instance_ids=[0,1] \
-#     use_heavy_robot=true \
-#     max_steps=9000 \
-#     n_ds_steps=2300 \
-#     num_diffusion_steps=10 \
-#     inf_time_proprio_dropout=0.0 \
-#     log_path="${LOG_DIR}/mbts_train_0.0_pd_2200_ds"
+########################################################
+
+aws s3 sync \
+    s3://behavior-challenge/outputs/checkpoints/pi05_b1k_22_TASKS_oversample/openpi_05_20251113_045215/66000/ \
+    /workspace/openpi/outputs/checkpoints/pi05_b1k_22_TASKS_oversample/openpi_05_20251113_045215/66000/
+
+EXP_NAME="openpi_05_20251113_045215_66k_steps"
+LOG_DIR="video_outputs/${EXP_NAME}"
+
+mkdir -p "${LOG_DIR}"
+
+POLICY_ARGS="policy=local policy_config=pi05_b1k_22_TASKS_oversample policy_dir=/workspace/openpi/outputs/checkpoints/pi05_b1k_22_TASKS_oversample/openpi_05_20251113_045215/66000/"
+
+XLA_PYTHON_CLIENT_PREALLOCATE=false python OmniGibson/omnigibson/learning/eval.py \
+    ${POLICY_ARGS} \
+    task.name=moving_boxes_to_storage \
+    eval_on_train_instances=false \
+    eval_instance_ids=[2,3,4,5] \
+    use_heavy_robot=true \
+    max_steps=7200 \
+    inf_time_proprio_dropout=0.0 \
+    num_diffusion_steps=10 \
+    control_mode=receeding_horizon \
+    log_path="${LOG_DIR}/test_10_steps_receeding_horizon"
